fb3de9e85cce1a88a999cab87c07d173689df66a
katorec <kekemeciarova@gmail.com>
2024-04-25 12:11:39 +0200

    feat: univers of IOSvol2 disappering in the eternity of being whole

--- proj2/main.c
+++ proj2/main.c
@@ -1,355 +1,341 @@
 /**
-* @name     IOS projekt vol2
- * @author  Katarina Meciarova ~ xmeciak00
- * @date    18.4.2024
+    * @name     IOS projekt vol2
+    * @author  Katarina Meciarova ~ xmeciak00
+    * @date    18.4.2024 (updated: 24.4.2024)
 */
 
 #include "main.h"
 
+/// main function
+/// \param argc checks how many arguments were used
+/// \param argv checks what content in arguments were used
+/// \return 1 if failed, 0 if succeeded
 int main(int argc, char *argv[]){
     if (argc!=6) {
         perror("Too many or too little arguments, only 1+5 accepted.\n");
         exit (1);
     }
-
-        L  = atoi(argv[1]);
-        Z  = atoi(argv[2]);
-        K  = atoi(argv[3]);
-        TL = atoi(argv[4]);
-        TB = atoi(argv[5]);
-
-
+/// convert arguments to integers
+        L  = atoi(argv[1]); //skiers
+        Z  = atoi(argv[2]); //stops
+        K  = atoi(argv[3]); //capacity of the bus
+        TL = atoi(argv[4]); //maximum time to wait for skiers
+        TB = atoi(argv[5]); //maximum time to wait for the bus
+/// what if someone gives incompatible values? PERROR! ~ we do warn the user :) and exit with value 1
         if (!(skierMAX && stopsMAX && stopsMIN && busMIN && busMAX && WAITskier && WAITbus)){
             perror("Wrong values used, contact the author for more information.\n");
             exit (1);
         }
-
-        file = fopen("proj2.out", "w");
+/// initiate file for writing
+        file = fopen("proj2_11.4.10.4.5?74.out", "w");
         if (file == NULL) {
             perror("Error opening file.\n");
-            THEEND(0);
             exit (1);
         }
-
+/// initiate shared memory for needed variables as semaphores or integers meant for checking the "stav"
         int shmstart = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
         if (shmstart < 0) {
             perror("error shmget");
-            THEEND(90);
+            fclose(file);
             exit(1);
         }
         start = shmat(shmstart, NULL, 0);
         if (leave == (sem_t*) -1) {
             perror("error shmat");
-            THEEND(91);
+            fclose(file); // taking care of the memory, if failed
             exit(1);
         }
-        sem_init(start, 1, (0-(L+1)));
-
+        sem_init(start, 1, -L); // initiate semaphore meant for proper start ~ bus after skiers
 
         int shmstop = shmget(IPC_PRIVATE, sizeof(sem_t)*Z, IPC_CREAT | 0666);
         if (shmstop < 0) {
             perror("error shmget");
-            THEEND(80);
+            error1(); // taking care of the memory, if failed
             exit(1);
         }
         stop = shmat(shmstop, NULL, 0);
         if (stop == (sem_t*) -1) {
             perror("error shmat");
-            THEEND(81);
+            error1(); // taking care of the memory, if failed
             exit(1);
         }
-        //inicializujeme o par riadkov nizsie spolu so shm of stops
+        // we initiate the semaphore few lines below, with idZ and ARR trough for loop
 
         int shmleave = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
         if (shmleave < 0) {
             perror("error shmget");
-            THEEND(70);
+            error2(); // taking care of the memory, if failed
             exit(1);
         }
         leave = shmat(shmleave, NULL, 0);
         if (leave == (sem_t*) -1) {
             perror("error shmat");
-            THEEND(71);
+            error2(); // taking care of the memory, if failed
             exit(1);
         }
-        sem_init(leave, 1, 0);
+        sem_init(leave, 1, 0); // initiate semaphore meant for proper leave of the bus
 
         int shmfinal = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
         if (shmfinal < 0) {
             perror("error shmget");
-            THEEND(60);
+            error3(); // taking care of the memory, if failed
             exit(1);
         }
         final = shmat(shmfinal, NULL, 0);
         if (final == (sem_t*) -1) {
             perror("error shmat");
-            THEEND(61);
+            error3(); // taking care of the memory, if failed
             exit(1);
         }
-        sem_init(final, 1, 0);
+        sem_init(final, 1, 0); // initiate semaphore meant for proper arriving to the final
 
-        
         int shmA = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
         if (shmA < 0) {
             perror("error shmget");
-            THEEND(50);
+            error4(); // taking care of the memory, if failed
             exit(1);
         }
         A = shmat(shmA, NULL, 0);
         if (A == (int*) -1) {
             perror("error shmat");
-            THEEND(51);
+            error4(); // taking care of the memory, if failed
             exit(1);
         }
-        *A=0;
+        *A=0; // variable of the number of actions
 
         int shmidZ = shmget(IPC_PRIVATE, sizeof(int)*Z, IPC_CREAT | 0666);
         if (shmidZ < 0) {
             perror("error shmget");
-            THEEND(40);
+            error5(); // taking care of the memory, if failed
             exit(1);
         }
         idZ = shmat(shmidZ, NULL, 0);
         if (idZ == (int*) -1) {
             perror("error shmat");
-            THEEND(41);
+            error5(); // taking care of the memory, if failed
             exit(1);
         }
+        // we initiate the variable few lines below, with sem.stop and ARR trough for loop
 
-
-        int shmBOARD = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
-        if (shmBOARD < 0) {
-            perror("error shmget");
-            THEEND(30);
-            exit(1);
-        }
-        BOARD = shmat(shmBOARD, NULL, 0);
-        if (BOARD == (int*) -1) {
-            perror("error shmat");
-            THEEND(31);
-            exit(1);
-        }
-        *BOARD=0;
-
-        int shmARR = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+    int shmARR = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
         if (shmARR < 0) {
             perror("error shmget");
-            THEEND(20);
+            error6(); // taking care of the memory, if failed
             exit(1);
         }
         ARR = shmat(shmARR, NULL, 0);
         if (ARR == (int*) -1) {
             perror("error shmat");
-            THEEND(21);
+            error6(); // taking care of the memory, if failed
             exit(1);
         }
 
         for (int i=0; i<Z; i++){
-            idZ[i]=i+1;
-            sem_init(&stop[i], 1, 0);
-            ARR[i]=0;
+            idZ[i]=i+1; // variable meant for proper locating of skiers || bus trough id of stops
+            sem_init(&stop[i], 1, 0); // semaphore for stops ~ proper boarding etd
+            ARR[i]=0; // variable for counting skiers waiting on specific stop, diff by id of stops
         }
+
+        int shmBOARD = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+        if (shmBOARD < 0) {
+            perror("error shmget");
+            error7(); // taking care of the memory, if failed
+            exit(1);
+        }
+        BOARD = shmat(shmBOARD, NULL, 0);
+        if (BOARD == (int*) -1) {
+            perror("error shmat");
+            error7(); // taking care of the memory, if failed
+            exit(1);
+        }
+        *BOARD=0; // variable for counting skiers on board of the bus ~ controlled by constant variable of capacity
 
         int shmwaiting = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
         if (shmwaiting < 0) {
             perror("error shmget");
-            THEEND(10);
+            error8(); // taking care of the memory, if failed
             exit(1);
         }
         waiting = shmat(shmwaiting, NULL, 0);
         if (waiting == (int*) -1) {
             perror("error shmat");
-            THEEND(11);
+            error8(); // taking care of the memory, if failed
             exit(1);
         }
-        *waiting=L;
+        *waiting=L; // variable for counting skiers still waiting, not on their way to ski yet
 
-
+/// here we call functions as processes. "fork" takes care of having separated processes running on the "same" time
+    pid_t skibusP = fork();
+    if (skibusP == 0) {
+        skibus();
+        theEND();  // taking care of the memory before end, tbs
+        exit(0);
+    }
     for (int i = 1; i <= L; i++) {
         pid_t skierP = fork();
         if (skierP == 0) {
             skier(i);
-            THEEND(11);
+            theEND(); // taking care of the memory before end, tbs
             exit(0);
         }
     }
-
-    pid_t skibusP = fork();
-    if (skibusP == 0) {
-        skibus();
-        THEEND(11);
-        exit(0);
-    }
-
 
     for (int i = 0; i < L; i++) {
         wait(NULL);
     }
     wait(NULL);
 
-
-    if (!(*waiting==0 || *BOARD==0)){
-        perror("Processes failed.\n");
-        THEEND(1);
-        exit (1);
-    }
-
-
-    THEEND(1);
-
+    theEND(); // taking care of the memory once again, to be sure :)
     exit (0);
-
 }
 
+/// skibus function takes care of the process of skibus's process. It is operated by semaphores ~ synchronized with skiers
+void skibus(void){
 
-void THEEND(int in){
-    switch(in){
-        case 11:
-            shmdt(waiting);
-        case 10:
-            shmctl(shmwaiting, IPC_RMID, NULL);
+    sem_wait(start); // waits 'till all the skiers starts
 
-        case 21:
-            shmdt(ARR);
-        case 20:
-            shmctl(shmARR, IPC_RMID, NULL);
-
-        case 31:
-            shmdt(BOARD);
-        case 30:
-            shmctl(shmBOARD, IPC_RMID, NULL);
-
-        case 41:
-            shmdt(idZ);
-        case 40:
-            shmctl(shmidZ, IPC_RMID, NULL);
-
-        case 51:
-            shmdt(A);
-        case 50:
-            shmctl(shmA, IPC_RMID, NULL);
-
-
-
-        case 62:
-            sem_destroy(final);
-        case 61:
-            shmdt(final);
-        case 60:
-            shmctl(shmfinal, IPC_RMID, NULL);
-
-
-
-        case 72:
-            sem_destroy(leave);
-        case 71:
-            shmdt(leave);
-        case 70:
-            shmctl(shmleave, IPC_RMID, NULL);
-
-
-        case 82:
-            sem_destroy(stop);
-        case 81:
-            shmdt(stop);
-        case 80:
-            shmctl(shmstop, IPC_RMID, NULL);
-
-
-        case 92:
-            sem_destroy(start);
-        case 91:
-            shmdt(start);
-        case 90:
-            shmctl(shmstart, IPC_RMID, NULL);
-
-        default:
-            fclose(file);
-    }
-}
-
-void skibus(){
-
-    sem_wait(start);
-
-    //printf("%d: BUS: started \n", ++*A);
     fprintf(file,"%d: BUS: started \n", ++*A);
     fflush(file);
-    int id;
+    int id; // initiation of local variable meant for identification of stops
+/// this process~while operates until all of the skiers are not skiing already
     while (*waiting>0){
-
-        id=0;
+        id=0; // takes care of proper order of stops
+/// this while operates until the last stop (before final) is not reached
         while (id < Z){
-            srand(time(NULL) ^ (getpid()<<16));
-            usleep(rand() % TB);
+            srand(time(NULL) ^ (getpid()<<16)); // takes care of randomness
+            usleep(rand() % TB); // random choosing of the time bus needs to get to the stop[id]
 
-            //printf("%d: BUS: arrived to %d \n", ++*A, idZ[id]);
             fprintf(file, "%d: BUS: arrived to %d \n", ++*A, idZ[id]);
             fflush(file);
+/// if arrived, wait 'till all skiers on current stop boarded on bus
             while (*BOARD<K && ARR[id]>0){
-                sem_post(&stop[id]);
-                sem_wait(leave);
+                sem_post(&stop[id]); // gives signal to board
+                sem_wait(leave); // waits for signal to leave
             }
 
-            //printf("%d: BUS: leaving %d \n", ++*A, idZ[id++]);
             fprintf(file,"%d: BUS: leaving %d \n", ++*A, idZ[id++]);
             fflush(file);
         }
         usleep(rand() % TB);
 
-        //printf("%d: BUS: arrived to final \n", ++*A);
         fprintf(file, "%d: BUS: arrived to final \n", ++*A);
         fflush(file);
-        
+/// if on final, wait until every skier on board leaves
         while (*BOARD>0){
-            sem_post(final);
-            sem_wait(leave);
+            sem_post(final); // gives signal to leave the bus
+            sem_wait(leave); // waits for all the signal to leave the final stop and get on another way
         }
 
-        //printf("%d: BUS: leaving final \n", ++*A);
         fprintf(file, "%d: BUS: leaving final \n", ++*A);
         fflush(file);
     }
-
-    //printf("%d: BUS: finish \n", ++*A);
+/// if all af the skiers are on the hill skiing already
     fprintf(file, "%d: BUS: finish \n", ++*A);
     fflush(file);
 }
 
-
+/// skier function takes care of the process of skiers's processes. By semaphores ~ synchronized with skibus's process
+/// \param idL differentiate and identifies skiers
 void skier(int idL){
-    srand(time(NULL) ^ (getpid()<<16));
-    int id = rand() % Z;
+    srand(time(NULL) ^ (getpid()<<16)); // randomness
+    int id = rand() % Z; // random choosing of the stop for the skier idL
 
-    //printf("%d: L %d: started \n", ++*A, idL);
     fprintf(file, "%d: L %d: started \n", ++*A, idL);
     fflush(file);
 
-    
-    usleep(rand() % TL+0);
+/// if started, signal the bus
     sem_post(start);
 
-    //printf("%d: L %d: arrived to %d \n", ++*A, idL, idZ[id]);
+    usleep(rand() % TL+0); // random choosing of the time to walk to stop[id]
+
     fprintf(file, "%d: L %d: arrived to %d \n", ++*A, idL, idZ[id]);
     fflush(file);
-    ARR[id]+=1;
-   
+    ARR[id]+=1; // if arrived, increment the ARR[id], based on stop[id], to know, how many will board on stop[id]
+///   wait for the signal to board from the bus
     sem_wait(&stop[id]);
-    ARR[id]-=1;
+    ARR[id]-=1; // if signaled, decrement ARR[id]
 
-    //printf("%d: L %d: boarding \n", ++*A, idL);
     fprintf(file, "%d: L %d: boarding \n", ++*A, idL);
     fflush(file);
-    ++*BOARD;
+    ++*BOARD; // increment BORAD to know, how many will leave on the final stop
+/// signal the bus to leave
     sem_post(leave);
-
-
+/// wait for the signal to leave the board
     sem_wait(final);
-    --*BOARD;
+    --*BOARD; // if signaled, decrement BOARD
+    --*waiting; // also decrement waiting, so we know, if is someone still waiting for skiing
+/// if left the board, signal bus for leave
     sem_post(leave);
 
-    //printf("%d: L %d: going to ski \n", ++*A, idL);
     fprintf(file, "%d: L %d: going to ski \n", ++*A, idL);
     fflush(file);
-    --*waiting;
 }
+
+/// if shm_stop failed
+void error1(void){
+    sem_destroy(start);
+    shmdt(start);
+    shmctl(shmstart, IPC_RMID, NULL);
+    fclose(file);
+}
+
+/// if shm_leave failed
+void error2(void){
+    error1();
+    sem_destroy(stop);
+    shmdt(stop);
+    shmctl(shmstop, IPC_RMID, NULL);
+}
+
+/// if shm_final failed
+void error3(void){
+    error2();
+    sem_destroy(leave);
+    shmdt(leave);
+    shmctl(shmleave, IPC_RMID, NULL);
+}
+
+/// if shm_A failed
+void error4(void){
+    error3();
+    sem_destroy(final);
+    shmdt(final);
+    shmctl(shmfinal, IPC_RMID, NULL);
+}
+
+/// if shm_idZ failed
+void error5(void){
+    error4();
+    shmdt(A);
+    shmctl(shmA, IPC_RMID, NULL);
+}
+
+/// if shm_ARR failed
+void error6(void){
+    error5();
+    shmdt(idZ);
+    shmctl(shmidZ, IPC_RMID, NULL);
+}
+
+/// if shm_BOARD failed
+void error7(void){
+    error6();
+    shmdt(ARR);
+    shmctl(shmARR, IPC_RMID, NULL);
+}
+
+/// if shm_waiting failed
+void error8(void){
+    error7();
+    shmdt(BOARD);
+    shmctl(shmBOARD, IPC_RMID, NULL);
+}
+
+/// if the whole programs ends (success || fail)
+void theEND(){
+    error8();
+    shmdt(waiting);
+    shmctl(shmwaiting, IPC_RMID, NULL);
+}
+
+/// the END
--- proj2/main.h
+++ proj2/main.h
@@ -17,7 +17,6 @@
 #include <sys/shm.h>
 #include <sys/wait.h>
 
-
 #define skierMAX    (L < 20000)
 #define stopsMAX    (Z <= 10)
 #define stopsMIN    (Z > 0)
@@ -26,19 +25,26 @@
 #define WAITskier   ((0 <= TB)&&(TB <= 10000))
 #define WAITbus     ((0 <= TL)&&(TL <= 1000))
 
+//needed variables
 int L, Z, K, TL, TB, *A, *idZ, *ARR, *BOARD, *waiting;
-//int *idL; no need
-
 sem_t *start, *stop, *leave, *final;
-
 FILE *file;
-
 int shmstart, shmstop, shmleave, shmfinal, shmA, shmidZ, shmBOARD, shmARR, shmwaiting;
 
-
+//main functions, also some (skier && skibus) implemented as processes
 int main(int argc, char *argv[]);
 void skier(int idL);
-void skibus();
-void THEEND(int in);
+void skibus(void);
+
+// freeing the memory after errors, if happened
+void error1(void);
+void error2(void);
+void error3(void);
+void error4(void);
+void error5(void);
+void error6(void);
+void error7(void);
+void error8(void);
+void theEND(void);
 
 #endif
