34bb528c146ba904314cbf1740285fc1f5048546
katorec <kekemeciarova@gmail.com>
2024-04-25 16:13:36 +0200

    feat: univers of IOSvol2 ETERNITYY

--- proj2/main.c
+++ proj2/main.c
@@ -22,41 +22,29 @@ int main(int argc, char *argv[]){
         TL = atoi(argv[4]); //maximum time to wait for skiers
         TB = atoi(argv[5]); //maximum time to wait for the bus
 /// what if someone gives incompatible values? PERROR! ~ we do warn the user :) and exit with value 1
-        if (!(skierMAX && stopsMAX && stopsMIN && busMIN && busMAX && WAITskier && WAITbus)){
+        if (!(skierMIN && skierMAX && stopsMAX && stopsMIN && busMIN && busMAX && WskierMIN && WskierMAX && WbusMIN && WbusMAX)){
             perror("Wrong values used, contact the author for more information.\n");
             exit (1);
         }
+        //TODO chceck for integers
 /// initiate file for writing
-        file = fopen("proj2_11.4.10.4.5?74.out", "w");
+        file = fopen("proj2.out", "w");
         if (file == NULL) {
             perror("Error opening file.\n");
             exit (1);
         }
 /// initiate shared memory for needed variables as semaphores or integers meant for checking the "stav"
-        int shmstart = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
-        if (shmstart < 0) {
-            perror("error shmget");
-            fclose(file);
-            exit(1);
-        }
-        start = shmat(shmstart, NULL, 0);
-        if (leave == (sem_t*) -1) {
-            perror("error shmat");
-            fclose(file); // taking care of the memory, if failed
-            exit(1);
-        }
-        sem_init(start, 1, -L); // initiate semaphore meant for proper start ~ bus after skiers
 
         int shmstop = shmget(IPC_PRIVATE, sizeof(sem_t)*Z, IPC_CREAT | 0666);
         if (shmstop < 0) {
             perror("error shmget");
-            error1(); // taking care of the memory, if failed
+                fclose(file); // taking care of the memory, if failed
             exit(1);
         }
         stop = shmat(shmstop, NULL, 0);
         if (stop == (sem_t*) -1) {
             perror("error shmat");
-            error1(); // taking care of the memory, if failed
+                fclose(file); // taking care of the memory, if failed
             exit(1);
         }
         // we initiate the semaphore few lines below, with idZ and ARR trough for loop
@@ -64,13 +52,19 @@ int main(int argc, char *argv[]){
         int shmleave = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
         if (shmleave < 0) {
             perror("error shmget");
-            error2(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         leave = shmat(shmleave, NULL, 0);
         if (leave == (sem_t*) -1) {
             perror("error shmat");
-            error2(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         sem_init(leave, 1, 0); // initiate semaphore meant for proper leave of the bus
@@ -78,13 +72,25 @@ int main(int argc, char *argv[]){
         int shmfinal = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0666);
         if (shmfinal < 0) {
             perror("error shmget");
-            error3(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         final = shmat(shmfinal, NULL, 0);
         if (final == (sem_t*) -1) {
             perror("error shmat");
-            error3(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         sem_init(final, 1, 0); // initiate semaphore meant for proper arriving to the final
@@ -92,13 +98,31 @@ int main(int argc, char *argv[]){
         int shmA = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
         if (shmA < 0) {
             perror("error shmget");
-            error4(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL);
+                sem_destroy(final);
+                shmdt(final);
+                shmctl(shmfinal, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         A = shmat(shmA, NULL, 0);
         if (A == (int*) -1) {
             perror("error shmat");
-            error4(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL);
+                sem_destroy(final);
+                shmdt(final);
+                shmctl(shmfinal, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         *A=0; // variable of the number of actions
@@ -106,13 +130,35 @@ int main(int argc, char *argv[]){
         int shmidZ = shmget(IPC_PRIVATE, sizeof(int)*Z, IPC_CREAT | 0666);
         if (shmidZ < 0) {
             perror("error shmget");
-            error5(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL);
+                sem_destroy(final);
+                shmdt(final);
+                shmctl(shmfinal, IPC_RMID, NULL);
+                shmdt(A);
+                shmctl(shmA, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         idZ = shmat(shmidZ, NULL, 0);
         if (idZ == (int*) -1) {
             perror("error shmat");
-            error5(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL);
+                sem_destroy(final);
+                shmdt(final);
+                shmctl(shmfinal, IPC_RMID, NULL);
+                shmdt(A);
+                shmctl(shmA, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         // we initiate the variable few lines below, with sem.stop and ARR trough for loop
@@ -120,13 +166,39 @@ int main(int argc, char *argv[]){
     int shmARR = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
         if (shmARR < 0) {
             perror("error shmget");
-            error6(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL);
+                sem_destroy(final);
+                shmdt(final);
+                shmctl(shmfinal, IPC_RMID, NULL);
+                shmdt(A);
+                shmctl(shmA, IPC_RMID, NULL);
+                shmdt(idZ);
+                shmctl(shmidZ, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         ARR = shmat(shmARR, NULL, 0);
         if (ARR == (int*) -1) {
             perror("error shmat");
-            error6(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL);
+                sem_destroy(final);
+                shmdt(final);
+                shmctl(shmfinal, IPC_RMID, NULL);
+                shmdt(A);
+                shmctl(shmA, IPC_RMID, NULL);
+                shmdt(idZ);
+                shmctl(shmidZ, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
 
@@ -139,13 +211,43 @@ int main(int argc, char *argv[]){
         int shmBOARD = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
         if (shmBOARD < 0) {
             perror("error shmget");
-            error7(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL);
+                sem_destroy(final);
+                shmdt(final);
+                shmctl(shmfinal, IPC_RMID, NULL);
+                shmdt(A);
+                shmctl(shmA, IPC_RMID, NULL);
+                shmdt(idZ);
+                shmctl(shmidZ, IPC_RMID, NULL);
+                shmdt(ARR);
+                shmctl(shmARR, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         BOARD = shmat(shmBOARD, NULL, 0);
         if (BOARD == (int*) -1) {
             perror("error shmat");
-            error7(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL);
+                sem_destroy(final);
+                shmdt(final);
+                shmctl(shmfinal, IPC_RMID, NULL);
+                shmdt(A);
+                shmctl(shmA, IPC_RMID, NULL);
+                shmdt(idZ);
+                shmctl(shmidZ, IPC_RMID, NULL);
+                shmdt(ARR);
+                shmctl(shmARR, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         *BOARD=0; // variable for counting skiers on board of the bus ~ controlled by constant variable of capacity
@@ -153,13 +255,47 @@ int main(int argc, char *argv[]){
         int shmwaiting = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
         if (shmwaiting < 0) {
             perror("error shmget");
-            error8(); // taking care of the memory, if failed
+                fclose(file);
+                sem_destroy(stop);
+                shmdt(stop);
+                shmctl(shmstop, IPC_RMID, NULL);
+                sem_destroy(leave);
+                shmdt(leave);
+                shmctl(shmleave, IPC_RMID, NULL);
+                sem_destroy(final);
+                shmdt(final);
+                shmctl(shmfinal, IPC_RMID, NULL);
+                shmdt(A);
+                shmctl(shmA, IPC_RMID, NULL);
+                shmdt(idZ);
+                shmctl(shmidZ, IPC_RMID, NULL);
+                shmdt(ARR);
+                shmctl(shmARR, IPC_RMID, NULL);
+                shmdt(BOARD);
+                shmctl(shmBOARD, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         waiting = shmat(shmwaiting, NULL, 0);
         if (waiting == (int*) -1) {
             perror("error shmat");
-            error8(); // taking care of the memory, if failed
+            fclose(file);
+            sem_destroy(stop);
+            shmdt(stop);
+            shmctl(shmstop, IPC_RMID, NULL);
+            sem_destroy(leave);
+            shmdt(leave);
+            shmctl(shmleave, IPC_RMID, NULL);
+            sem_destroy(final);
+            shmdt(final);
+            shmctl(shmfinal, IPC_RMID, NULL);
+            shmdt(A);
+            shmctl(shmA, IPC_RMID, NULL);
+            shmdt(idZ);
+            shmctl(shmidZ, IPC_RMID, NULL);
+            shmdt(ARR);
+            shmctl(shmARR, IPC_RMID, NULL);
+            shmdt(BOARD);
+            shmctl(shmBOARD, IPC_RMID, NULL); // taking care of the memory, if failed
             exit(1);
         }
         *waiting=L; // variable for counting skiers still waiting, not on their way to ski yet
@@ -168,32 +304,48 @@ int main(int argc, char *argv[]){
     pid_t skibusP = fork();
     if (skibusP == 0) {
         skibus();
-        theEND();  // taking care of the memory before end, tbs
         exit(0);
     }
+
     for (int i = 1; i <= L; i++) {
         pid_t skierP = fork();
         if (skierP == 0) {
             skier(i);
-            theEND(); // taking care of the memory before end, tbs
             exit(0);
         }
     }
+
 
     for (int i = 0; i < L; i++) {
         wait(NULL);
     }
     wait(NULL);
 
-    theEND(); // taking care of the memory once again, to be sure :)
+    fclose(file);
+    sem_destroy(stop);
+    shmdt(stop);
+    shmctl(shmstop, IPC_RMID, NULL);
+    sem_destroy(leave);
+    shmdt(leave);
+    shmctl(shmleave, IPC_RMID, NULL);
+    sem_destroy(final);
+    shmdt(final);
+    shmctl(shmfinal, IPC_RMID, NULL);
+    shmdt(A);
+    shmctl(shmA, IPC_RMID, NULL);
+    shmdt(idZ);
+    shmctl(shmidZ, IPC_RMID, NULL);
+    shmdt(ARR);
+    shmctl(shmARR, IPC_RMID, NULL);
+    shmdt(BOARD);
+    shmctl(shmBOARD, IPC_RMID, NULL);
+    shmdt(waiting);
+    shmctl(shmwaiting, IPC_RMID, NULL); // taking care of the memory once again, to be sure :)
     exit (0);
 }
 
 /// skibus function takes care of the process of skibus's process. It is operated by semaphores ~ synchronized with skiers
 void skibus(void){
-
-    sem_wait(start); // waits 'till all the skiers starts
-
     fprintf(file,"%d: BUS: started \n", ++*A);
     fflush(file);
     int id; // initiation of local variable meant for identification of stops
@@ -243,9 +395,6 @@ void skier(int idL){
     fprintf(file, "%d: L %d: started \n", ++*A, idL);
     fflush(file);
 
-/// if started, signal the bus
-    sem_post(start);
-
     usleep(rand() % TL+0); // random choosing of the time to walk to stop[id]
 
     fprintf(file, "%d: L %d: arrived to %d \n", ++*A, idL, idZ[id]);
@@ -269,73 +418,6 @@ void skier(int idL){
 
     fprintf(file, "%d: L %d: going to ski \n", ++*A, idL);
     fflush(file);
-}
-
-/// if shm_stop failed
-void error1(void){
-    sem_destroy(start);
-    shmdt(start);
-    shmctl(shmstart, IPC_RMID, NULL);
-    fclose(file);
-}
-
-/// if shm_leave failed
-void error2(void){
-    error1();
-    sem_destroy(stop);
-    shmdt(stop);
-    shmctl(shmstop, IPC_RMID, NULL);
-}
-
-/// if shm_final failed
-void error3(void){
-    error2();
-    sem_destroy(leave);
-    shmdt(leave);
-    shmctl(shmleave, IPC_RMID, NULL);
-}
-
-/// if shm_A failed
-void error4(void){
-    error3();
-    sem_destroy(final);
-    shmdt(final);
-    shmctl(shmfinal, IPC_RMID, NULL);
-}
-
-/// if shm_idZ failed
-void error5(void){
-    error4();
-    shmdt(A);
-    shmctl(shmA, IPC_RMID, NULL);
-}
-
-/// if shm_ARR failed
-void error6(void){
-    error5();
-    shmdt(idZ);
-    shmctl(shmidZ, IPC_RMID, NULL);
-}
-
-/// if shm_BOARD failed
-void error7(void){
-    error6();
-    shmdt(ARR);
-    shmctl(shmARR, IPC_RMID, NULL);
-}
-
-/// if shm_waiting failed
-void error8(void){
-    error7();
-    shmdt(BOARD);
-    shmctl(shmBOARD, IPC_RMID, NULL);
-}
-
-/// if the whole programs ends (success || fail)
-void theEND(){
-    error8();
-    shmdt(waiting);
-    shmctl(shmwaiting, IPC_RMID, NULL);
 }
 
 /// the END
--- proj2/main.h
+++ proj2/main.h
@@ -17,34 +17,26 @@
 #include <sys/shm.h>
 #include <sys/wait.h>
 
+#define skierMIN    (L > 0)
 #define skierMAX    (L < 20000)
 #define stopsMAX    (Z <= 10)
 #define stopsMIN    (Z > 0)
 #define busMIN      (K >= 10)
 #define busMAX      (K <= 100)
-#define WAITskier   ((0 <= TB)&&(TB <= 10000))
-#define WAITbus     ((0 <= TL)&&(TL <= 1000))
+#define WbusMIN     (0 <= TB)
+#define WbusMAX     (TB <= 1000)
+#define WskierMIN   (0 <= TL)
+#define WskierMAX   (TL <= 10000)
 
 //needed variables
 int L, Z, K, TL, TB, *A, *idZ, *ARR, *BOARD, *waiting;
 sem_t *start, *stop, *leave, *final;
 FILE *file;
-int shmstart, shmstop, shmleave, shmfinal, shmA, shmidZ, shmBOARD, shmARR, shmwaiting;
 
 //main functions, also some (skier && skibus) implemented as processes
 int main(int argc, char *argv[]);
 void skier(int idL);
 void skibus(void);
 
-// freeing the memory after errors, if happened
-void error1(void);
-void error2(void);
-void error3(void);
-void error4(void);
-void error5(void);
-void error6(void);
-void error7(void);
-void error8(void);
-void theEND(void);
 
 #endif
